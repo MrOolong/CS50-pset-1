73deec957c200e3b9276c241bb340c47160a2e58
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-10-30 22:33:55 +0000

    completed greedy.c

--- greedy.c
+++ greedy.c
@@ -1,30 +1,45 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
 
 int main (void){
 
+//avoid imprecision by using modulo? use math.h's round() function
 //use get_float() to prompt user input
 float change; //var to use for prompting user input
-float quarter = 0.25;
-float dime = 0.10;
-float nickel = 0.05;
-float penny = 0.00;
-int coinInt = 0;
+int coins, cents;
 
-
-do {
-    printf("How much change is owed?\n");
+    printf("O hai! How much change is owed?\n");
         change = get_float();
-    printf("You're owed %f \n\n", change);
-}
+    //printf("You're owed %f \n\n", change); //test print to see float amount
 
-while ( change >= 0.01); {
 
-    for ()
-    if(change == 0) {
-    printf("Number of coins to produce change: %i \n\n", coinInt);
-    //printf("%f \n",change);
-    }
+while ( change < 0)
+;
+{
+    //convert to cents
+    cents = (int) round(change * 100);
+
+    coins = 0;
+
+    //check quarters
+    coins = coins + cents / 25;
+    cents %= 25;
+
+    //check dimes
+    coins = coins + cents / 10;
+    cents %= 10;
+
+    //check nickels
+    coins = coins + cents / 5;
+    cents %= 5;
+
+    //check pennies
+    coins = coins + cents;
+
+        printf("%i\n", coins);
+
+
 };
 
 }//closing brace
